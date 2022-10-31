9232374cc3a68ead10666a528d2ec5c169244e2f
Mason <mason.rozumny23@d155.org>
2022-10-31 08:41:01 -0500

    Initial Commit

---
+++ ShoppingCartProgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCartProgram
+//
+//  Created by MASON ROZUMNY on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
